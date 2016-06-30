552c066eb7b7e0bbafc8640d3aa6f742bde976b7
cj <cj@cj-macbook-pro.local>
2016-06-30 11:42:22 -0400

    Initial Commit

---
+++ Black and White/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Black and White
+//
+//  Created by cj on 16/6/30.
+//  Copyright © 2016年 Carleton University Math Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Black and WhiteTests/Black_and_WhiteTests.m
@@ -0,0 +1,39 @@
+//
+//  Black_and_WhiteTests.m
+//  Black and WhiteTests
+//
+//  Created by cj on 16/6/30.
+//  Copyright © 2016年 Carleton University Math Lab. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Black_and_WhiteTests : XCTestCase
+
+@end
+
+@implementation Black_and_WhiteTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
